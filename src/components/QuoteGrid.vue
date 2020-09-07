24c6ff5a451a03b69a0b50903c3904e9db36b594
Tame <tame@Eyoatams-MacBook-Pro.local>
2020-09-07 12:08:41 +0300

    initial commit

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <app-quote
+      v-for="(quote, index) in quotes"
+      @click.native="deleteQuote(index)"
+      class="mb-4"
+      :key="quote"
+    >{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote,
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
